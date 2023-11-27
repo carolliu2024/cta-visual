42ff4f1404aa0bf3484c321759b22502f9cb21aa
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2023-11-26 23:20:31 -0600

    change aesthetic

--- src/mapScript.js
+++ src/mapScript.js
@@ -15,18 +15,34 @@ const projection = d3.geoMercator()
     .scale(60000) // Zoom in on Chicagoland region
     .translate([width / 2, height / 2]);
 
-// Create a path generator
-const path = d3.geoPath().projection(projection);
+// Set up the tile generator
+const tile = d3.tile()
+  .size([width, height])
+  .scale(projection.scale()* 2 * Math.PI) // * 2 * Math.PI
+  .translate(projection([0, 0]));
 
-d3.json('illinois-counties.geojson').then(illinois => {
-    // Draw the map with debugging styles
-    svg.selectAll('path')
-        .data(illinois.features)
-        .enter().append('path')
-        .attr('d', path)
-        .style('fill', 'none') // Set fill to none for debugging
-        .style('stroke', 'red'); // Set a red stroke color for debugging
-});
+// Generate tiles
+const tiles = tile();
+
+// Assuming you have a function 'url' for generating tile URLs
+function url(x, y, z) {
+  // Return tile URL
+  // return `https://tile.openstreetmap.org/${z}/${x}/${y}.png`;
+  return `https://tiles.stadiamaps.com/tiles/stamen_toner_lite/${z}/${x}/${y}.png`
+}
+
+// Create a path generator
+// const path = d3.geoPath().projection(projection);
+
+// d3.json('illinois-counties.geojson').then(illinois => {
+//     // Draw the map with debugging styles
+//     svg.selectAll('path')
+//         .data(illinois.features)
+//         .enter().append('path')
+//         .attr('d', path)
+//         .style('fill', 'none') // Set fill to none for debugging
+//         .style('stroke', 'red'); // Set a red stroke color for debugging
+// });
 
 // Load your CSV data with ridership information
 d3.csv('ridership_with_locs-2.csv').then(data => {
@@ -49,6 +65,20 @@ d3.csv('ridership_with_locs-2.csv').then(data => {
     const colorScale = d3.scaleSequential(d3.interpolateBlues)
         .domain([0, d3.max(aggregatedData, d => d.value.totalRidership)]);
 
+
+    // Append image elements for each tile
+    svg.selectAll('image')
+        .data(tiles)
+        .enter().append('image')
+        .attr('xlink:href', d => url(d[0], d[1], d[2]))
+        .attr('x', d => Math.round((d[0] + tiles.translate[0]) * tiles.scale))
+        .attr('y', d => Math.round((d[1] + tiles.translate[1]) * tiles.scale))
+        .attr('width', tiles.scale)
+        .attr('height', tiles.scale);
+
+    // Example of using the map in the document
+    document.body.appendChild(svg.node());
+
     // Map the aggregated data to the stations on the map
     svg.selectAll('circle')
         .data(aggregatedData)
@@ -60,6 +90,7 @@ d3.csv('ridership_with_locs-2.csv').then(data => {
         .style('opacity', 0.7) // Adjust the circle opacity
         .style('stroke', 'black') // Set a black stroke color for debugging
         .style('stroke-width', 1); // Set a stroke width for debugging
+
 });
 
 
